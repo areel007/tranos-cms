9acd8ac0872b72648d14050be2f9017f1dccf0c4
Sunday Morenikeji <65197762+areel007@users.noreply.github.com>
2022-11-22 10:26:24 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer')
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+// Middleware
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.post('/', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'delzmiyaki@gmail.com',
+      pass: 'gwnqbqiyjwtmnces',
+    }
+  })
+
+  const mailOptions = {
+    from: req.body.email,
+    to: 'delzmiyaki@gmail.com',
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: `${req.body.message}`
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('Error')
+    } else {
+      console.log('Email sent ' + info.response);
+      res.send('Success')
+    }
+  })
+})
+
+app.listen(PORT, () => console.log('App running on ' + PORT))
